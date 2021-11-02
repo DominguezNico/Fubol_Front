98ef971c5dbb051345f0544af886131602d4b902
Nico <domingueznicoo@hotmail.com>
2021-11-01 22:20:23 -0300

    Prueba redireccionar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,49 @@
 import React from "react";
-import {BrowserRouter as Router,Route,Switch,Link,Redirect} from "react-router-dom";
+import {BrowserRouter as Router,Route,Switch,Link,Redirect,render} from "react-router-dom";
 import { Navbar,Container,Nav } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import {hasRole,isAllowed} from './auth';
+
+import { browserHistory } from 'react-router';
 
 import Home from './paginas/Home'
 import Login from './paginas/Login'
 import PageNotFound from './paginas/PageNotFound'
+import Jugador from './componentes/Jugador/inicioJugador'
+import Admin from './componentes/Admin/inicioAdmin'
+import Representante from './componentes/Representante/inicioRepr'
 
 import ClubComponent from "./componentes/ClubComponent";
+import { useHistory } from "react-router-dom";/*
+const jugador= {
+  roles: ['jugador'],
+  rights: ['jugador']
+};
 
+const representante ={
+  roles: ['representante'],
+  rights: ['representante']
+}
 
+const admin = {
+  roles: ['admin'],
+  rights:['admin']
+};*/
+
+/*
+let history = useHistory();
+  function changePage() {
+  
+    this.props.history.push("/login");
+  }
+*/
 
 function App() {
+  
   return (
     <Router>
       <div className="App">
+      
         <Navbar bg="dark" variant="dark">
           <Container>
           <Navbar.Brand href="/">TP_APIS</Navbar.Brand>
@@ -22,17 +51,18 @@ function App() {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/features">Features</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
-            <Nav.Link href="/ClubComponent">Clubes</Nav.Link>
           </Nav>
           </Container>
         </Navbar>
-        
+        {/*<button type="button" onClick={changePage}>Go to a New Page!</button>*/}
         <Switch>
-        <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route  exact path="/ClubComponent" component={ClubComponent} />
-          <Route component={PageNotFound} />
         
+          <Route exact path="/login" component={Login} />
+          <Route component={PageNotFound} />
+         {/* {hasRole(user,['jugador']) && <Route path='/jugador' component={Jugador}/>} 
+          {hasRole(user,['admin']) && <Route path='/admin' component={Admin}/>}
+          {hasRole(user,['representante']) && <Route path='/representante' component={Representante}/>}*/}
+          <Route exact path="/" component={Home} />
         </Switch>
 
 
