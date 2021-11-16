123ac3ac46b787e861fbb449c4fba2f099744667
FlorVernucci <florvernucci@gmail.com>
2021-11-16 15:51:33 -0300

    avance 3

--- frontend/src/componentes/Admin/campeonatos/crearCampeonato.js
+++ frontend/src/componentes/Admin/campeonatos/crearCampeonato.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from "react";
+
+import { BsKeyFill,BsFillPersonFill} from "react-icons/bs";
+import {BrowserRouter, Route, Switch,useHistory} from "react-router-dom";
+import {createBrowserHistory} from "history";
 
 
+function CrearCampeonato () {
 
-const crearCampeonato =props => {
-
-
+  const inicialEstadoParams ={
+    descrip:"",
+    fechaInicio:"",
+    fechaFin:"",
+    estado:"",
+  };
 
   const [descrip,setDescrip]=useState(inicialEstadoParams);
   const [fechaInicio,setfechaInicio]=useState(inicialEstadoParams);
@@ -13,16 +21,84 @@ const crearCampeonato =props => {
 
 
 
-  const crearCamp =  (descripcion,fechaInicio,fechaFin,estado) => {
-    await fetch(`http://localhost:8080/crearCampeonato?descripcion=${descripcion}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}&estado=${estado}`)
-    console.log('creado')
+  const handledescripIChange = (e) => {
+    setDescrip(e.target.value);
   };
 
-    return(
-     <h2></h2>
-     
+const handlefechaInicioChange = (e) => {
+  setfechaInicio(e.target.value);
+  };
+
+  const handledesetFechaFinChange = (e) => {
+    setFechaFin(e.target.value);
+  };
+
+const handlesetEstadoChange = (e) => {
+  setEstado(e.target.value);
+  };
+
+
+
+  const crearCamp =  async () => {
+    fetch(`http://localhost:8080/crearCampeonato?descripcion=${descrip}&fechaInicio=${fechaInicio}&fechaFin=${fechaFin}&estado=${estado}`, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+   
+  })
+    
+  };
+
   
+
+    return(
+      <div className="container">
+       <div className="d-flex justify-content-center h-100">
+        <div className="card">
+          <div className="card-header">
+          <div className="card-body">
+            <form>
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend"></div>
+                <input type="text" id="doc" className="form-control" placeholder="descripcion"  onChange={handledescripIChange} />
+              </div>
+
+
+
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend"></div>
+                <input type="fechaInicio" className="form-control" placeholder="fechaInicio"  onChange={handlefechaInicioChange}/>
+              </div>
+
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend"> </div>
+                <input type="fechaFin" className="form-control" placeholder="fechaFin"  onChange={handledesetFechaFinChange}/>
+              </div>
+
+
+
+              <div className="input-group form-group">
+                <div className="input-group-prepend"> </div>
+                <input type="estado" className="form-control" placeholder="estado"  onChange={handlesetEstadoChange}/>
+              </div>
+
+
+
+              <div className="form-group">
+                <input type="Button" value="crearCamp" className="btn btn-primary col-lg-5 mx-1 mb-1" onClick={crearCamp}/>
+              </div>
+
+          </form>
+
+          </div>
+
+        </div>
+      </div>
+      </div>
+      </div>
     )
   
   }
-  export default crearCampeonato
+  export default CrearCampeonato
